82ae995c3bb6068ba5afd460ae146a12d2951463
chiragjind <133591077+chiragjind@users.noreply.github.com>
2023-12-19 16:16:11 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import TT_Pack from "./component/techteam";
 import Navbar from "./component/Navbar/Navbar";
-// import Mainbody from "./component/Mainbody/Mainbody";
+import Mainbody from "./component/Mainbody/Mainbody";
 import Page1 from "./component/Chirag/Page1";
 import "./component/Chirag/sytle1.css";
 import "./component/Chirag/style2.css";
 import "./component/Chirag/style3.css";
+import "./component/Chirag/magicscroll/magicscroll.css"
 function App() {
   return (
     <>
       <div className="mainpage">
         <Navbar />
-        {/* <Mainbody /> */}
+        <Mainbody />
       </div>
       <div className="page">
         <Page1 />
---
+++ src/component/Chirag/Page6.js
@@ -0,0 +1,112 @@
+import React from 'react'
+
+function Page6() {
+  return (
+    <>
+      <div class="emain_container">
+                              <div class="ecenter_div">
+                                <div class="MagicScroll lastclass" data-options="mode: cover-flow; height: 500px;">
+                                 
+                    <div class="card3">
+                      <div class="imgbox3">
+                          <img src="imagess/img1i.jpg" alt=""/>
+                      </div>
+                      <div class="content3">
+                          <h2>Keep SMiling</h2>
+                          <p> Lorem, ipsum Lorem ipsum dolor sit. <br/>dolor.maxime molestiae, quo,</p>
+                      </div>
+                  </div>
+                  
+                    <div class="card3">
+                      <div class="imgbox3">
+                          <img src="imagess/img2i.jpg" alt=""/>
+                      </div>
+                      <div class="content3">
+                          <h2>Keep SMiling</h2>
+                          <p> Lorem, ipsum Lorem ipsum dolor sit. <br/>dolor.maxime molestiae, quo,</p>
+                      </div>
+                  </div>
+                  
+                    <div class="card3">
+                      <div class="imgbox3">
+                          <img src="imagess/img3i.jpg" alt=""/>
+                      </div>
+                      <div class="content3">
+                          <h2>Keep SMiling</h2>
+                          <p> Lorem, ipsum Lorem ipsum dolor sit. <br/>dolor.maxime molestiae, quo,</p>
+                      </div>
+                  </div>
+                  
+                    <div class="card3">
+                      <div class="imgbox3">
+                          <img src="imagess/img4i.jpg" alt=""/>
+                      </div>
+                      <div class="content3">
+                          <h2>Keep SMiling</h2>
+                          <p> Lorem, ipsum Lorem ipsum dolor sit. <br/>dolor.maxime molestiae, quo,</p>
+                      </div>
+                  </div>
+                    <div class="card3">
+                      <div class="imgbox3">
+                          <img src="imagess/img5i.jpg" alt=""/>
+                      </div>
+                      <div class="content3">
+                          <h2>Keep SMiling</h2>
+                          <p> Lorem, ipsum Lorem ipsum dolor sit. <br/>dolor.maxime molestiae, quo,</p>
+                      </div>
+                  </div>
+                    <div class="card3">
+                      <div class="imgbox3">
+                          <img src="imagess/img6i.png" alt=""/>
+                      </div>
+                      <div class="content3">
+                          <h2>Keep SMiling</h2>
+                          <p> Lorem, ipsum Lorem ipsum dolor sit. <br/>dolor.maxime molestiae, quo,</p>
+                      </div>
+                  </div>
+                    <div class="card3">
+                      <div class="imgbox3">
+                          <img src="imagess/img7i.jpg" alt=""/>
+                      </div>
+                      <div class="content3">
+                          <h2>Keep SMiling</h2>
+                          <p> Lorem, ipsum Lorem ipsum dolor sit. <br/>dolor.maxime molestiae, quo,</p>
+                      </div>
+                  </div>
+                    <div class="card3">
+                      <div class="imgbox3">
+                          <img src="imagess/img8i.jpg" alt=""/>
+                      </div>
+                      <div class="content3">
+                          <h2>Keep SMiling</h2>
+                          <p> Lorem, ipsum Lorem ipsum dolor sit. <br/>dolor.maxime molestiae, quo,</p>
+                      </div>
+                  </div>
+                    <div class="card3">
+                      <div class="imgbox3">
+                          <img src="imagess/img9i.jpg" alt=""/>
+                      </div>
+                      <div class="content3">
+                          <h2>Keep SMiling</h2>
+                          <p> Lorem, ipsum Lorem ipsum dolor sit. <br/>dolor.maxime molestiae, quo,</p>
+                      </div>
+                  </div>
+                    <div class="card3">
+                      <div class="imgbox3">
+                          <img src="imagess/img10i.jpg" alt=""/>
+                      </div>
+                      <div class="content3">
+                          <h2>Keep SMiling</h2>
+                          <p> Lorem, ipsum Lorem ipsum dolor sit. <br/>dolor.maxime molestiae, quo,</p>
+                      </div>
+                  </div>
+                  
+                  
+                              </div>
+                              </div>
+                            </div>
+    </>
+  )
+}
+
+export default Page6
