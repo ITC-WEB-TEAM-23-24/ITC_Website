2ab03f1655b6b24d17a87a68ffdf5423153e1dca
Daksh-99 <22b2150@iitb.ac.in>
2023-12-19 12:41:48 +0530

    background

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ import Page1 from "./component/Chirag/Page1";
 import "./component/Chirag/sytle1.css";
 import "./component/Chirag/style2.css";
 import "./component/Chirag/style3.css";
+import video from "./video.mp4";
+import video1 from "./video1.mp4";
+import video2 from "./video2.mp4";
 function App() {
   return (
     <>
+      <video autoPlay loop muted id="video">
+        <source src={video2} type="video/mp4" />
+      </video>
       <div className="mainpage">
         <Navbar />
         <Mainbody />
