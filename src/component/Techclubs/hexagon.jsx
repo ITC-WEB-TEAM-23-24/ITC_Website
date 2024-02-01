f224aba415d2eec924254e9e1f1bde9d9d2c93d0
Ishan Patni <ishanpatni123@gmail.com>
2024-02-01 08:10:48 +0530

    changes

--- src/component/Techclubs/hexagon.jsx
+++ src/component/Techclubs/hexagon.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import styles from "./techclubs.module.css";
 
-const Hexagon = ({ name, description, imageUrl, onClick }) => {
+const Hexagon = ({ name, imageUrl, onClick, animate }) => {
+    // Animation variants
+    const variants = {
+        initial: { rotateY: 0 },
+        animate: { rotateY: 90 }
+    };
+
     return (
-        <div className={styles.techClubsArticle} onClick={() => onClick(name)}>
+        <motion.div 
+            className={styles.techClubsArticle}
+            onClick={onClick}
+            variants={variants}
+            animate={animate ? "animate" : "initial"}
+            transition={{ duration: 0.3 }}
+        >
             <img src={imageUrl} alt={name} className={styles.techClubsImg}/>
             <div className={styles.techclubname}>{name}</div>
-        </div>
+        </motion.div>
     );
 };
 
